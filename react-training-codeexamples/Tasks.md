369d13f7442248ee8addda3bc7625208285471c9
Anna-Maria Auer <anna-maria.auer@capgemini.com>
2022-05-09 10:50:56 +0200

    tsx example

---
+++ react-training-codeexamples/Tasks.md
@@ -0,0 +1,5 @@
+# Tasks
+
+## JSX / TSX
+1. Create a Book-Component which displays the author's name, title of the book and it's release date
+2. Render the component in App.tsx
---
+++ react-training-codeexamples/src/components/book/Book.tsx
@@ -0,0 +1,11 @@
+const Book: React.FC = () => {
+  return (
+    <div>
+      <h2>Moby Dick</h2>
+      <p>Author: Herman Melville</p>
+      <p>Release Date: 1851</p>
+    </div>
+  );
+};
+
+export default Book;
