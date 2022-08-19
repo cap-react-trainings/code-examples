e42f7374dc3d19c951af0dfa33bd32230d42417f
Ludwig Hubert (Capgemini) <ludwig.hubert@capgemini.com>
2022-08-19 09:59:09 +0200

    Extract book list to separate component

--- react-training-codeexamples/src/App.tsx
+++ react-training-codeexamples/src/App.tsx
@@ -1,76 +1,19 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import BookItem from "./components/book/Book";
-import LoadingWrapper from "./components/loading-wrapper/LoadingWrapper";
-import BookSlider from "./components/slider/BookSlider";
-
-export interface Book {
-  title: string;
-  subtitle: string;
-  isbn13: string;
-  price: string;
-  image: string;
-  url: string;
-}
+import BookList from "./components/book/BookList";
 
 function App() {
-  const [books, setBooks] = useState<Book[]>();
-  const [selectedNumberOfBooks, setSelectedNumberOfBooks] = useState<number>();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://api.itbook.store/1.0/new")
-      .then((response) => response.json())
-      .then((data) => {
-        setBooks(data.books);
-      })
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
-  return (
-    <div className="App">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <div style={{ width: "50vw" }}>
-          <h1 style={{ marginBottom: 12 }}>Booklist</h1>
-          <div style={{ marginTop: 12 }}>
-            <LoadingWrapper loading={loading}>
-              {books && (
-                <>
-                  <div style={{ marginTop: 2 }}>
-                    <BookSlider
-                      numberOfBooks={books.length}
-                      onSliderChange={(value: number) =>
-                        setSelectedNumberOfBooks(value)
-                      }
-                    />
-                  </div>
-                  <div style={{ marginTop: 4 }}>
-                    {books
-                      .slice(0, selectedNumberOfBooks)
-                      .map((book, index) => (
-                        <BookItem book={book} key={index} />
-                      ))}
-                  </div>
-                </>
-              )}
-              {!books && (
-                <div>
-                  <p style={{ marginBottom: 4 }}>
-                    Sorry, we couldn't find any books to display! 😞
-                  </p>
-                </div>
-              )}
-            </LoadingWrapper>
-          </div>
+    return (
+        <div className="App">
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                }}
+            >
+                <BookList />
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ react-training-codeexamples/src/components/book/BookList.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import LoadingWrapper from "../loading-wrapper/LoadingWrapper";
+import BookSlider from "../slider/BookSlider";
+import BookItem from "./Book";
+
+export interface Book {
+    title: string;
+    subtitle: string;
+    isbn13: string;
+    price: string;
+    image: string;
+    url: string;
+}
+
+interface BookListProps {}
+
+const BookList: React.FC<BookListProps> = (props: BookListProps) => {
+    const [books, setBooks] = useState<Book[]>();
+    const [selectedNumberOfBooks, setSelectedNumberOfBooks] = useState<number>();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("https://api.itbook.store/1.0/new")
+            .then((response) => response.json())
+            .then((data) => {
+                setBooks(data.books);
+            })
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+    return (
+        <div style={{ width: "50vw" }}>
+            <h1 style={{ marginBottom: 12 }}>Booklist</h1>
+            <div style={{ marginTop: 12 }}>
+                <LoadingWrapper loading={loading}>
+                    {books && (
+                        <>
+                            <div style={{ marginTop: 2 }}>
+                                <BookSlider
+                                    numberOfBooks={books.length}
+                                    onSliderChange={(value: number) => setSelectedNumberOfBooks(value)}
+                                />
+                            </div>
+                            <div style={{ marginTop: 4 }}>
+                                {books.slice(0, selectedNumberOfBooks).map((book, index) => (
+                                    <BookItem book={book} key={index} />
+                                ))}
+                            </div>
+                        </>
+                    )}
+                    {!books && (
+                        <div>
+                            <p style={{ marginBottom: 4 }}>Sorry, we couldn't find any books to display! 😞</p>
+                        </div>
+                    )}
+                </LoadingWrapper>
+            </div>
+        </div>
+    );
+};
+
+export default BookList;
