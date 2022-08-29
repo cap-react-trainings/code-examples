145afe76bebbee50cbfbd06ec38a99e736d3d955
Ludwig Hubert (Capgemini) <ludwig.hubert@capgemini.com>
2022-08-29 13:56:17 +0200

    Add test for book component

---
+++ react-training-codeexamples/src/components/book/Book.test.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import BookItem from "./Book";
+
+const testDataCheap = {
+    title: "title",
+    subtitle: "subtitle",
+    isbn13: "isbn13",
+    price: "€15.0",
+    image: "image",
+    url: "url",
+};
+
+const testDataExpensive = {
+    title: "title",
+    subtitle: "subtitle",
+    isbn13: "isbn13",
+    price: "€50.0",
+    image: "image",
+    url: "url",
+};
+
+test("shows book with cheap badge", async () => {
+    render(<BookItem book={testDataCheap} />);
+    const badge = await screen.findByText("cheap");
+    expect(badge).toBeTruthy();
+});
+
+test("shows book with cheap badge", async () => {
+    render(<BookItem book={testDataExpensive} />);
+    const badge = await screen.findByText("expensive");
+    expect(badge).toBeTruthy();
+});
