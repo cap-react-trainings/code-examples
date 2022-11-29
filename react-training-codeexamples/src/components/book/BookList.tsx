aa74c5c8b531a39943bc57275e1adc17cb187b66
Anna-Maria Auer <anna-maria.auer@capgemini.com>
2022-11-29 09:24:16 +0100

    exchanged slider with buttons

--- react-training-codeexamples/src/components/book/BookList.tsx
+++ react-training-codeexamples/src/components/book/BookList.tsx
@@ -1,7 +1,24 @@
 import React, { useEffect, useState } from "react";
 import LoadingWrapper from "../loading-wrapper/LoadingWrapper";
-import BookSlider from "../slider/BookSlider";
 import BookItem from "./Book";
+
+interface ButtonProps {
+  displayNumber: number;
+  onClick: (val: number) => void;
+  active: boolean;
+}
+
+const NumberButton: React.FC<ButtonProps> = (props: ButtonProps) => {
+  return (
+    <button
+      className={`numberButton ${props.active ? "active" : ""}`}
+      onClick={() => props.onClick(props.displayNumber)}
+    >
+      {props.displayNumber}
+    </button>
+  );
+};
+
 
 export interface Book {
     title: string;
@@ -12,52 +29,62 @@ export interface Book {
     url: string;
 }
 
-interface BookListProps {}
+const BookList: React.FC = () => {
+  const [books, setBooks] = useState<Book[]>();
+  const [selectedNumberOfBooks, setSelectedNumberOfBooks] = useState<number>();
+  const displayNumbers = [0, 1, 3];
+  const [loading, setLoading] = useState(true);
 
-const BookList: React.FC<BookListProps> = (props: BookListProps) => {
-    const [books, setBooks] = useState<Book[]>();
-    const [selectedNumberOfBooks, setSelectedNumberOfBooks] = useState<number>();
-    const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.itbook.store/1.0/new")
+      .then((response) => response.json())
+      .then((data) => {
+        setBooks(data.books);
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
 
-    useEffect(() => {
-        fetch("https://api.itbook.store/1.0/new")
-            .then((response) => response.json())
-            .then((data) => {
-                setBooks(data.books);
-            })
-            .catch((error) => console.error(error))
-            .finally(() => setLoading(false));
-    }, []);
-
-    return (
-        <div style={{ width: "50vw" }}>
-            <h1 style={{ marginBottom: 12 }}>Booklist</h1>
-            <div style={{ marginTop: 12 }}>
-                <LoadingWrapper loading={loading}>
-                    {books && (
-                        <>
-                            <div style={{ marginTop: 2 }}>
-                                <BookSlider
-                                    numberOfBooks={books.length}
-                                    onSliderChange={(value: number) => setSelectedNumberOfBooks(value)}
-                                />
-                            </div>
-                            <div style={{ marginTop: 4 }}>
-                                {books.slice(0, selectedNumberOfBooks).map((book, index) => (
-                                    <BookItem book={book} key={index} />
-                                ))}
-                            </div>
-                        </>
-                    )}
-                    {!books && (
-                        <div>
-                            <p style={{ marginBottom: 4 }}>Sorry, we couldn't find any books to display! 😞</p>
-                        </div>
-                    )}
-                </LoadingWrapper>
+  return (
+    <div style={{ width: "50vw" }}>
+      <h1 style={{ marginBottom: 12 }}>Booklist</h1>
+      <div style={{ marginTop: 12 }}>
+        <LoadingWrapper loading={loading}>
+          {books && (
+            <>
+              <div style={{ marginTop: 2 }}>
+                {displayNumbers.map((num) => (
+                  <NumberButton
+                    displayNumber={num}
+                    onClick={(val: number) => setSelectedNumberOfBooks(val)}
+                    active={num === selectedNumberOfBooks}
+                  />
+                ))}
+                <button
+                  className="numberButton"
+                  onClick={() => setSelectedNumberOfBooks(undefined)}
+                >
+                  ⇠
+                </button>
+              </div>
+              <div style={{ marginTop: 4 }}>
+                {books.slice(0, selectedNumberOfBooks).map((book, index) => (
+                  <BookItem book={book} key={index} />
+                ))}
+              </div>
+            </>
+          )}
+          {!books && (
+            <div>
+              <p style={{ marginBottom: 4 }}>
+                Sorry, we couldn't find any books to display! 😞
+              </p>
             </div>
-        </div>
-    );
+          )}
+        </LoadingWrapper>
+      </div>
+    </div>
+  );
 };
 
 export default BookList;
