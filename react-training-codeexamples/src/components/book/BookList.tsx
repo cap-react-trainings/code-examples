df9e3da818db0ceaa844598ba507a32d9283e10f
Anna-Maria Auer <anna-maria.auer@capgemini.com>
2022-11-29 09:21:46 +0100

    exchangec book slider with buttons

--- react-training-codeexamples/src/components/book/BookList.tsx
+++ react-training-codeexamples/src/components/book/BookList.tsx
@@ -1,71 +1,97 @@
 import React, { useContext, useEffect, useState } from "react";
 import { DarkModeContext } from "../../App";
 import LoadingWrapper from "../loading-wrapper/LoadingWrapper";
-import BookSlider from "../slider/BookSlider";
 import BookItem from "./Book";
 
-export interface Book {
-    title: string;
-    subtitle: string;
-    isbn13: string;
-    price: string;
-    image: string;
-    url: string;
+interface ButtonProps {
+  displayNumber: number;
+  onClick: (val: number) => void;
+  active: boolean;
 }
 
-interface BookListProps {}
+const NumberButton: React.FC<ButtonProps> = (props: ButtonProps) => {
+  return (
+    <button
+      className={`numberButton ${props.active ? "active" : ""}`}
+      onClick={() => props.onClick(props.displayNumber)}
+    >
+      {props.displayNumber}
+    </button>
+  );
+};
 
-const BookList: React.FC<BookListProps> = (props: BookListProps) => {
-    const { isDarkModeEnabled, toggleDarkMode } = useContext(DarkModeContext);
-    const [books, setBooks] = useState<Book[]>();
-    const [selectedNumberOfBooks, setSelectedNumberOfBooks] = useState<number>();
-    const [loading, setLoading] = useState(true);
+export interface Book {
+  title: string;
+  subtitle: string;
+  isbn13: string;
+  price: string;
+  image: string;
+  url: string;
+}
 
-    useEffect(() => {
-        fetch("https://api.itbook.store/1.0/new")
-            .then((response) => response.json())
-            .then((data) => {
-                setBooks(data.books);
-            })
-            .catch((error) => console.error(error))
-            .finally(() => setLoading(false));
-    }, []);
+const BookList: React.FC = () => {
+  const { isDarkModeEnabled, toggleDarkMode } = useContext(DarkModeContext);
+  const [books, setBooks] = useState<Book[]>();
+  const [selectedNumberOfBooks, setSelectedNumberOfBooks] = useState<number>();
+  const displayNumbers = [0, 1, 3];
+  const [loading, setLoading] = useState(true);
 
-    return (
-        <div
-            style={{
-                width: "50vw",
-                color: isDarkModeEnabled ? "white" : "black",
-            }}
-        >
-            <h1 style={{ marginBottom: 12 }}>Booklist</h1>
-            <button onClick={() => toggleDarkMode()}>Switch Mode</button>
-            <div style={{ marginTop: 12 }}>
-                <LoadingWrapper loading={loading}>
-                    {books && (
-                        <>
-                            <div style={{ marginTop: 2 }}>
-                                <BookSlider
-                                    numberOfBooks={books.length}
-                                    onSliderChange={(value: number) => setSelectedNumberOfBooks(value)}
-                                />
-                            </div>
-                            <div style={{ marginTop: 4 }}>
-                                {books.slice(0, selectedNumberOfBooks).map((book, index) => (
-                                    <BookItem book={book} key={index} />
-                                ))}
-                            </div>
-                        </>
-                    )}
-                    {!books && (
-                        <div>
-                            <p style={{ marginBottom: 4 }}>Sorry, we couldn't find any books to display! 😞</p>
-                        </div>
-                    )}
-                </LoadingWrapper>
+  useEffect(() => {
+    fetch("https://api.itbook.store/1.0/new")
+      .then((response) => response.json())
+      .then((data) => {
+        setBooks(data.books);
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: "50vw",
+        color: isDarkModeEnabled ? "white" : "black",
+      }}
+    >
+      <h1 style={{ marginBottom: 12 }}>Booklist</h1>
+      <button onClick={() => toggleDarkMode()}>Switch Mode</button>
+      <div style={{ marginTop: 12 }}>
+        <LoadingWrapper loading={loading}>
+          {books && (
+            <>
+              <div style={{ marginTop: 2 }}>
+                {displayNumbers.map((num) => (
+                  <NumberButton
+                    displayNumber={num}
+                    onClick={(val: number) => setSelectedNumberOfBooks(val)}
+                    active={num === selectedNumberOfBooks}
+                  />
+                ))}
+                <button
+                  className="numberButton"
+                  onClick={() => setSelectedNumberOfBooks(undefined)}
+                >
+                  ⇠
+                </button>
+              </div>
+              <div style={{ marginTop: 4 }}>
+                {books.slice(0, selectedNumberOfBooks).map((book, index) => (
+                  <BookItem book={book} key={index} />
+                ))}
+              </div>
+            </>
+          )}
+          {!books && (
+            <div>
+              <p style={{ marginBottom: 4 }}>
+                Sorry, we couldn't find any books to display! 😞
+              </p>
             </div>
-        </div>
-    );
+          )}
+        </LoadingWrapper>
+      </div>
+    </div>
+  );
 };
 
 export default BookList;
