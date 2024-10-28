f82a369a51dc6e8cd8a76bd22f6bcea96ce22798
Alexander Wrede <alexander.wrede@capgemini.com>
2024-10-28 16:26:25 +0100

    04-context-wrapper-update-deps

--- react-training-codeexamples/src/components/book/Book.tsx
+++ react-training-codeexamples/src/components/book/Book.tsx
@@ -1,10 +1,11 @@
+import { FunctionComponent } from "react";
 import { Book } from "./BookList";
 
 interface Props {
     book: Book;
 }
 
-const BookItem: React.FC<Props> = ({ book }: Props) => {
+const BookItem: FunctionComponent<Props> = ({ book }: Props) => {
     const isCheap = parseFloat(book.price.substring(1, book.price.length)) < 30;
     return (
         <div
